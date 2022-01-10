866ac43862d978c8659706d375d0f4d7e9b217de
go-roc <1743299@qq.com>
2022-01-10 19:56:01 +0800

    fix: log size

--- internal/transport/rsocket/server.go
+++ internal/transport/rsocket/server.go
@@ -16,328 +16,330 @@
 package rs
 
 import (
-    ctx "context"
-    "runtime"
-    "sync"
+	ctx "context"
+	"runtime"
+	"sync"
 
-    "github.com/go-roc/roc/x"
-    "github.com/jjeffcaii/reactor-go/scheduler"
-    "github.com/rsocket/rsocket-go"
-    "github.com/rsocket/rsocket-go/payload"
-    "github.com/rsocket/rsocket-go/rx"
-    "github.com/rsocket/rsocket-go/rx/flux"
-    "github.com/rsocket/rsocket-go/rx/mono"
+	"github.com/jjeffcaii/reactor-go/scheduler"
+	"github.com/rsocket/rsocket-go"
+	"github.com/rsocket/rsocket-go/payload"
+	"github.com/rsocket/rsocket-go/rx"
+	"github.com/rsocket/rsocket-go/rx/flux"
+	"github.com/rsocket/rsocket-go/rx/mono"
 
-    "github.com/go-roc/roc/parcel/context"
-    "github.com/go-roc/roc/service/handler"
-    "github.com/go-roc/roc/service/router"
+	"github.com/go-roc/roc/x"
 
-    "github.com/go-roc/roc/parcel"
-    "github.com/go-roc/roc/rlog"
+	"github.com/go-roc/roc/parcel/context"
+	"github.com/go-roc/roc/service/handler"
+	"github.com/go-roc/roc/service/router"
+
+	"github.com/go-roc/roc/parcel"
+	"github.com/go-roc/roc/rlog"
 )
 
 type server struct {
 
-    //wait server run success
-    wg *sync.WaitGroup
+	//wait server run success
+	wg *sync.WaitGroup
 
-    //given serverName to service discovery to find
-    serverName string
+	//given serverName to service discovery to find
+	serverName string
 
-    //tcp socket address
-    tcpAddress string
+	//tcp socket address
+	tcpAddress string
 
-    //websocket address
-    wssAddress string
+	//websocket address
+	wssAddress string
 
-    //requestChannel buffSize setting
-    buffSize int
+	//requestChannel buffSize setting
+	buffSize int
 
-    //rsocket serverBuilder
-    serverBuilder rsocket.ServerBuilder
+	//rsocket serverBuilder
+	serverBuilder rsocket.ServerBuilder
 
-    //rsocket serverStarter
-    serverStart rsocket.ToServerStarter
+	//rsocket serverStarter
+	serverStart rsocket.ToServerStarter
 
-    dog []handler.DogHandler
+	dog []handler.DogHandler
 }
 
 func (r *server) Address() string {
-    return "[tcp: " + r.tcpAddress + "] [wss: " + r.wssAddress + "]"
+	return "[tcp: " + r.tcpAddress + "] [wss: " + r.wssAddress + "]"
 }
 
 func (r *server) String() string {
-    return "rsocket"
+	return "rsocket"
 }
 
 func NewServer(tcpAddress, wssAddress, serverName string, buffSize int, dog ...handler.DogHandler) *server {
-    return &server{
-        serverName: serverName,
-        tcpAddress: tcpAddress,
-        wssAddress: wssAddress,
-        buffSize:   buffSize,
-        dog:        dog,
-    }
+	return &server{
+		serverName: serverName,
+		tcpAddress: tcpAddress,
+		wssAddress: wssAddress,
+		buffSize:   buffSize,
+		dog:        dog,
+	}
 }
 
 func (r *server) Accept(route *router.Router) {
-    r.serverBuilder = rsocket.Receive().OnStart(
-        func() {
-            r.wg.Done()
-        },
-    )
+	r.serverBuilder = rsocket.Receive().OnStart(
+		func() {
+			rlog.Debug("server start success")
+			r.wg.Done()
+		},
+	)
 
-    r.serverBuilder.Scheduler(
-        scheduler.NewElastic(runtime.NumCPU()<<8),
-        scheduler.NewElastic(runtime.NumCPU()<<8),
-    ) // setting scheduler goroutine on numCPU*2 to better working
+	r.serverBuilder.Scheduler(
+		scheduler.NewElastic(runtime.NumCPU()<<8),
+		scheduler.NewElastic(runtime.NumCPU()<<8),
+	) // setting scheduler goroutine on numCPU*2 to better working
 
-    r.serverBuilder.Resume()
-    r.serverStart = r.serverBuilder.
-        Acceptor(
-            func(
-                cc ctx.Context,
-                setup payload.SetupPayload,
-                sendingSocket rsocket.CloseableRSocket,
-            ) (rsocket.RSocket, error) {
+	r.serverBuilder.Resume()
+	r.serverStart = r.serverBuilder.
+		Acceptor(
+			func(
+				cc ctx.Context,
+				setup payload.SetupPayload,
+				sendingSocket rsocket.CloseableRSocket,
+			) (rsocket.RSocket, error) {
 
-                var c = context.Background()
-                var remoteIp, _ = rsocket.GetAddr(sendingSocket)
+				var c = context.Background()
+				var remoteIp, _ = rsocket.GetAddr(sendingSocket)
 
-                if len(r.dog) > 0 {
+				if len(r.dog) > 0 {
 
-                    c.SetSetupData(setup.Data())
+					c.SetSetupData(setup.Data())
 
-                    for i := range r.dog {
-                        rsp, err := r.dog[i](c)
-                        if err != nil {
-                            c.Errorf("dog reject you |message=%s", c.Codec().MustEncodeString(rsp))
-                            return nil, err
-                        }
-                    }
-                }
+					for i := range r.dog {
+						rsp, err := r.dog[i](c)
+						if err != nil {
+							c.Errorf("dog reject you |message=%s", c.Codec().MustEncodeString(rsp))
+							return nil, err
+						}
+					}
+				}
 
-                return rsocket.NewAbstractSocket(
-                    setupRequestResponse(route, remoteIp, setup),
-                    setupRequestStream(route, remoteIp, setup),
-                    setupRequestChannel(route, remoteIp, r.buffSize, setup),
-                ), nil
-            },
-        )
+				return rsocket.NewAbstractSocket(
+					setupRequestResponse(route, remoteIp, setup),
+					setupRequestStream(route, remoteIp, setup),
+					setupRequestChannel(route, remoteIp, r.buffSize, setup),
+				), nil
+			},
+		)
 }
 
 func (r *server) Run(wg *sync.WaitGroup) {
-    r.wg = wg
-    if r.tcpAddress != "" {
-        wg.Add(1)
-        r.tcp()
-    }
+	r.wg = wg
+	if r.tcpAddress != "" {
+		wg.Add(1)
+		r.tcp()
+	}
 
-    if r.wssAddress != "" {
-        wg.Add(1)
-        r.wss()
-    }
+	if r.wssAddress != "" {
+		wg.Add(1)
+		r.wss()
+	}
 }
 
 //run tcp socket server
 func (r *server) tcp() {
-    go func() {
-        err := r.serverStart.Transport(
-            rsocket.
-                TCPServer().
-                SetAddr(r.tcpAddress).
-                Build(),
-        ).Serve(ctx.TODO())
+	go func() {
+		err := r.serverStart.Transport(
+			rsocket.
+				TCPServer().
+				SetAddr(r.tcpAddress).
+				Build(),
+		).Serve(ctx.TODO())
 
-        if err != nil {
-            panic(err)
-        }
-    }()
+		if err != nil {
+			panic(err)
+		}
+	}()
 }
 
 //run websocket server
 func (r *server) wss() {
-    go func() {
-        err := r.serverStart.Transport(
-            rsocket.
-                WebsocketServer().
-                SetAddr(r.wssAddress).
-                Build(),
-        ).Serve(ctx.TODO())
+	go func() {
+		err := r.serverStart.Transport(
+			rsocket.
+				WebsocketServer().
+				SetAddr(r.wssAddress).
+				Build(),
+		).Serve(ctx.TODO())
 
-        if err != nil {
-            panic(err)
-        }
-    }()
+		if err != nil {
+			panic(err)
+		}
+	}()
 }
 
 // get metadata ignore error
 func mustGetMetadata(p payload.Payload) []byte {
-    b, _ := p.Metadata()
-    return b
+	b, _ := p.Metadata()
+	return b
 }
 
 func setupRequestResponse(r *router.Router, remoteIp string, setup payload.SetupPayload) rsocket.OptAbstractSocket {
-    return rsocket.RequestResponse(
-        func(p payload.Payload) mono.Mono {
+	return rsocket.RequestResponse(
+		func(p payload.Payload) mono.Mono {
 
-            var req, rsp = parcel.Payload(p.Data()), parcel.NewPacket()
-            defer func() {
-                parcel.Recycle(req, rsp)
-            }()
+			var req, rsp = parcel.Payload(p.Data()), parcel.NewPacket()
+			defer func() {
+				parcel.Recycle(req, rsp)
+			}()
 
-            c, err := context.FromMetadata(mustGetMetadata(p), setup.DataMimeType(), setup.MetadataMimeType())
-            if err != nil {
-                rlog.Errorf("err=%v |metadata=%s |mimeType=%s", err, x.BytesToString(mustGetMetadata(p)), setup.MetadataMimeType())
-                return mono.JustOneshot(payload.New(r.Error().Error400(c), nil))
-            }
+			c, err := context.FromMetadata(mustGetMetadata(p), setup.DataMimeType(), setup.MetadataMimeType())
+			if err != nil {
+				rlog.Errorf("err=%v |metadata=%s |mimeType=%s", err, x.BytesToString(mustGetMetadata(p)), setup.MetadataMimeType())
+				return mono.JustOneshot(payload.New(r.Error().Error400(c), nil))
+			}
 
-            c.RemoteAddr = remoteIp
+			c.RemoteAddr = remoteIp
 
-            err = r.RRProcess(c, req, rsp)
+			err = r.RRProcess(c, req, rsp)
 
-            if err == router.ErrNotFoundHandler {
-                c.Errorf("err=%v |path=%s", err, c.Method())
-                return mono.JustOneshot(payload.New(r.Error().Error404(c), nil))
-            }
+			if err == router.ErrNotFoundHandler {
+				c.Errorf("err=%v |path=%s", err, c.Method())
+				return mono.JustOneshot(payload.New(r.Error().Error404(c), nil))
+			}
 
-            if err != nil && rsp.Len() > 0 {
-                c.Error(err)
-                return mono.JustOneshot(payload.New(rsp.Bytes(), nil))
-            }
+			if err != nil && rsp.Len() > 0 {
+				c.Error(err)
+				return mono.JustOneshot(payload.New(rsp.Bytes(), nil))
+			}
 
-            if err != nil {
-                c.Error(err)
-                return mono.JustOneshot(payload.New(r.Error().Error400(c), nil))
-            }
+			if err != nil {
+				c.Error(err)
+				return mono.JustOneshot(payload.New(r.Error().Error400(c), nil))
+			}
 
-            return mono.JustOneshot(payload.New(rsp.Bytes(), nil))
-        },
-    )
+			return mono.JustOneshot(payload.New(rsp.Bytes(), nil))
+		},
+	)
 }
 
 func (r *server) Close() {
-    return
+	return
 }
 
 func setupRequestStream(router *router.Router, remoteIp string, setup payload.SetupPayload) rsocket.OptAbstractSocket {
-    return rsocket.RequestStream(
-        func(p payload.Payload) flux.Flux {
+	return rsocket.RequestStream(
+		func(p payload.Payload) flux.Flux {
 
-            return flux.Create(
-                func(ctx ctx.Context, sink flux.Sink) {
-                    var (
-                        req = parcel.Payload(p.Data())
-                    )
+			return flux.Create(
+				func(ctx ctx.Context, sink flux.Sink) {
+					var (
+						req = parcel.Payload(p.Data())
+					)
 
-                    c, err := context.FromMetadata(mustGetMetadata(p), setup.DataMimeType(), setup.MetadataMimeType())
-                    if err != nil {
-                        rlog.Errorf("err=%v |metadata=%s |mimeType=%s", err, x.BytesToString(mustGetMetadata(p)), setup.MetadataMimeType())
-                        return
-                    }
+					c, err := context.FromMetadata(mustGetMetadata(p), setup.DataMimeType(), setup.MetadataMimeType())
+					if err != nil {
+						rlog.Errorf("err=%v |metadata=%s |mimeType=%s", err, x.BytesToString(mustGetMetadata(p)), setup.MetadataMimeType())
+						return
+					}
 
-                    c.RemoteAddr = remoteIp
+					c.RemoteAddr = remoteIp
 
-                    //if you want to Disconnect channel
-                    //you must close rsp from server handler
-                    //this way is very friendly to closing channel transport
-                    rsp, err := router.RSProcess(c, req)
+					//if you want to Disconnect channel
+					//you must close rsp from server handler
+					//this way is very friendly to closing channel transport
+					rsp, err := router.RSProcess(c, req)
 
-                    //todo cannot know when socket will close to close(rsp)
-                    //you must close rsp at where send
+					//todo cannot know when socket will close to close(rsp)
+					//you must close rsp at where send
 
-                    parcel.Recycle(req)
+					parcel.Recycle(req)
 
-                    if err != nil {
-                        c.Errorf("transport CC failure |method=%s |err=%v", c.Method(), err)
-                        return
-                    }
+					if err != nil {
+						c.Errorf("transport CC failure |method=%s |err=%v", c.Method(), err)
+						return
+					}
 
-                    for b := range rsp {
-                        data, e := c.Codec().Encode(b)
-                        if e != nil {
-                            c.Errorf("transport CC Encode failure |method=%s |err=%v", c.Method(), err)
-                            continue
-                        }
-                        sink.Next(payload.New(data, nil))
-                    }
-                    sink.Complete()
-                },
-            )
-        },
-    )
+					for b := range rsp {
+						data, e := c.Codec().Encode(b)
+						if e != nil {
+							c.Errorf("transport CC Encode failure |method=%s |err=%v", c.Method(), err)
+							continue
+						}
+						sink.Next(payload.New(data, nil))
+					}
+					sink.Complete()
+				},
+			)
+		},
+	)
 }
 
 func setupRequestChannel(router *router.Router, remoteIp string, buffSize int, setup payload.SetupPayload) rsocket.OptAbstractSocket {
-    return rsocket.RequestChannel(
-        func(f flux.Flux) flux.Flux {
-            var (
-                req      = make(chan *parcel.RocPacket, buffSize)
-                exitRead = make(chan struct{})
-            )
+	return rsocket.RequestChannel(
+		func(f flux.Flux) flux.Flux {
+			var (
+				req      = make(chan *parcel.RocPacket, buffSize)
+				exitRead = make(chan struct{})
+			)
 
-            //read data from client by channel transport method
-            f.SubscribeOn(scheduler.Parallel()).
-                DoFinally(
-                    func(s rx.SignalType) {
-                        close(req)
-                        close(exitRead)
-                    },
-                ).
-                Subscribe(
-                    ctx.Background(),
-                    rx.OnNext(
-                        func(p payload.Payload) error {
-                            req <- parcel.Payload(payload.Clone(p).Data())
-                            return nil
-                        },
-                    ),
-                    rx.OnError(
-                        //if client is occurred error
-                        func(e error) {
-                            rlog.Errorf("setupRequestChannel OnError |err=%v", e)
-                        },
-                    ),
-                )
+			//read data from client by channel transport method
+			f.SubscribeOn(scheduler.Parallel()).
+				DoFinally(
+					func(s rx.SignalType) {
+						close(req)
+						close(exitRead)
+					},
+				).
+				Subscribe(
+					ctx.Background(),
+					rx.OnNext(
+						func(p payload.Payload) error {
+							req <- parcel.Payload(payload.Clone(p).Data())
+							return nil
+						},
+					),
+					rx.OnError(
+						//if client is occurred error
+						func(e error) {
+							rlog.Errorf("setupRequestChannel OnError |err=%v", e)
+						},
+					),
+				)
 
-            return flux.Create(
-                func(ctx ctx.Context, sink flux.Sink) {
+			return flux.Create(
+				func(ctx ctx.Context, sink flux.Sink) {
 
-                    var meta []byte
-                    for b := range req {
-                        meta = b.Bytes()
-                        rlog.Debugf("requestChanel success |ip=%s |meta=%s", remoteIp, string(meta))
-                        break
-                    }
+					var meta []byte
+					for b := range req {
+						meta = b.Bytes()
+						rlog.Debugf("requestChanel success |ip=%s |meta=%s", remoteIp, string(meta))
+						break
+					}
 
-                    c, err := context.FromMetadata(meta, setup.DataMimeType(), setup.MetadataMimeType())
-                    if err != nil {
-                        rlog.Errorf("err=%v |metadata=%s |mimeType=%s", err, x.BytesToString(meta), setup.MetadataMimeType())
-                        return
-                    }
+					c, err := context.FromMetadata(meta, setup.DataMimeType(), setup.MetadataMimeType())
+					if err != nil {
+						rlog.Errorf("err=%v |metadata=%s |mimeType=%s", err, x.BytesToString(meta), setup.MetadataMimeType())
+						return
+					}
 
-                    c.RemoteAddr = remoteIp
+					c.RemoteAddr = remoteIp
 
-                    //if you want to Disconnect channel
-                    //you must close rsp from server handler
-                    //this way is very friendly to closing channel transport
-                    rsp, err := router.RCProcess(c, req, exitRead)
-                    if err != nil {
-                        c.Errorf("transport CC failure |method=%s |err=%v", c.Method(), err)
-                        return
-                    }
+					//if you want to Disconnect channel
+					//you must close rsp from server handler
+					//this way is very friendly to closing channel transport
+					rsp, err := router.RCProcess(c, req, exitRead)
+					if err != nil {
+						c.Errorf("transport CC failure |method=%s |err=%v", c.Method(), err)
+						return
+					}
 
-                    for b := range rsp {
-                        data, e := c.Codec().Encode(b)
-                        if e != nil {
-                            c.Errorf("transport CC Encode failure |method=%s |err=%v", c.Method(), err)
-                            continue
-                        }
-                        sink.Next(payload.New(data, nil))
-                    }
-                    sink.Complete()
-                },
-            )
-        },
-    )
+					for b := range rsp {
+						data, e := c.Codec().Encode(b)
+						if e != nil {
+							c.Errorf("transport CC Encode failure |method=%s |err=%v", c.Method(), err)
+							continue
+						}
+						sink.Next(payload.New(data, nil))
+					}
+					sink.Complete()
+				},
+			)
+		},
+	)
 }
