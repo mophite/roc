82d80e562bb186341d8c47408c597e48bf37ace0
go-roc <1743299@qq.com>
2022-03-15 22:13:05 +0800

    feat； 更新

--- service/server/server.go
+++ service/server/server.go
@@ -29,6 +29,7 @@ import (
     "github.com/go-roc/roc/service/handler"
     "github.com/go-roc/roc/service/opt"
     "github.com/go-roc/roc/service/router"
+    "github.com/go-roc/roc/x"
 )
 
 type Server struct {
@@ -153,10 +154,12 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     }
 
     var c = context.New()
+    rlog.Debugf("---1--%s",x.MustMarshalString(c))
 
     handlerServerHttp(c, s, w, r)
-
+    rlog.Debugf("--2---%s",x.MustMarshalString(c))
     context.Recycle(c)
+    rlog.Debugf("--3---%s",x.MustMarshalString(c))
 }
 
 func (s *Server) CloseServer() {
